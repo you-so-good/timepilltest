368e0ea839981f57cf3850a8196a99b7785b66c7
Student <Student@Student-PC>
2024-05-13 17:42:27 +0900

    yes

--- ddd/src/main/java/kr/ac/kopo/Seokyoung.java
+++ ddd/src/main/java/kr/ac/kopo/Seokyoung.java
@@ -5,6 +5,7 @@ public class Seokyoung {
 	public static void main(String[] args) {
 		System.out.println("Seokyoung");
 		System.out.println("Seokyoung");
+		System.out.println("Seokyoung");
 	}
 
 }
